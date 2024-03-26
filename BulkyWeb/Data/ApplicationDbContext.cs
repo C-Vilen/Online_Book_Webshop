a6a92a09c4159e2dbab37e8273602cbd88638c06
C-Vilen <vile@itu.dk>
2024-03-26 18:01:36 +0100

    Added Category Controller and placeholder data through migration to database

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -13,5 +13,14 @@ namespace BulkyWeb.Data
 
         // Creating categories table in the database
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Drama", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "SciFi", DisplayOrder = 3 }
+                );
+        }
     }
 }
