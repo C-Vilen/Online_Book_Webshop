140057afe0afc51262b28b4703b39c51cf596114
C-Vilen <vile@itu.dk>
2024-03-26 17:22:29 +0100

    Connecting database from SQL Server and create categories table

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    // Inherits the .NET core DbContext class to query the database
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        // Creating categories table in the database
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // Specifying the primary-key id. [Key] is not need as .Net core will already treat it as a primary-key if you write Id or CategoryId.
+        [Key]
+        public int Id { get; set; }
+        // Saying Name is required to be filled out
+        [Required]
+        public string Name { get; set; }
+        public  int DisplayOrder { get; set; }
+    }
+}
